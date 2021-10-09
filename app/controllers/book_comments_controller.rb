ae6aeefe83adb06171d6371e334ef5fbb6bac891
supernemutai <tokikake66@gmail.com>
2021-10-09 05:47:32 +0000

    [Add]非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,20 +1,18 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    if comment.save
-     redirect_to book_path(book)
-    else
-     redirect_to book_path(book)
-    end
+    comment.book_id = @book.id
+    comment.save
+
   end
 
   def destroy
     @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
     BookComment.find_by(id: params[:id]).destroy
-    redirect_to book_path(@book.id)
   end
 
   private
